3bdedeb33b0b1fbb36e855cb05e8d7c29e3b3d24
yash766786 <yashchoudhary766786@gmail.com>
2024-10-11 17:10:51 +0530

    fixing small bugs

--- src/db/index.db.js
+++ src/db/index.db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constant";
+import { DB_NAME } from "../constant.js";
 
 const connectDB = async () =>{
     try {
      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+     console.log("db connected");
+     
     } 
     catch(error){
         process.exit(1);
